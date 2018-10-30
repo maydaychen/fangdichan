58a6945c29b0583941fec312c346c347ac84c4a0
chenyi <cy245832451@outlook.com>
2018-10-30 21:24:00 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,24 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+ 
+  onLoad: function () {
+    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
