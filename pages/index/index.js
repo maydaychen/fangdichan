283626698af74ded55e971435a5c17f7884c3466
chenyi <cy245832451@outlook.com>
2018-11-11 12:22:18 +0800

    钱包页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,49 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World'
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
- 
-  onLoad: function () {
+
+  onLoad: function() {
+    wx.login({
+      success: function(res) {
+        console.log(res);
+        if (res.code) {
+          wx.request({
+            url: 'https://apis.vitlf.com/user/getOpenId',
+            data: {
+              code: res.code
+            },
+            success(res) {
+              console.log(res.data)
+            },
+          })
+        } else {
+          wx.showModal({
+            title: '提示',
+            content: res.errMsg,
+            showCancel: false
+          })
+        }
+      },
+      fail: function(res) {
+        wx.showModal({
+          title: '提示',
+          content: res.errMsg,
+          showCancel: false
+        })
+      }
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
